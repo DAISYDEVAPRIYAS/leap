df4585938195d42c37f08abb65fe761cbf8a09c6
DAISYDEVAPRIYAS <christypriya2607@gmail.com>
2017-07-20 14:24:38 +0530

    Create leap_year.c

---
+++ leap_year.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	scanf("%d",&a);
+	if(a%400==0)
+	{
+		printf("Leap year");
+	}
+	else if(a%100==0)
+	{
+		printf("Not leap year");
+	}
+	else if(a%4==0)
+	{
+		printf("Leap year");
+	}
+	else
+	{
+		printf("Not leap year");
+	}
+	// your code goes here
+	return 0;
+}
